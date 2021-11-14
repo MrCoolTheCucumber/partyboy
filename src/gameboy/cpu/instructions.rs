3dc642386552d0661bdb1d44b285f53e78325453
Ruben <chickenzrage@gmail.com>
2021-11-14 01:11:55 +0000

    Implement some more instructions (0x22 to 0x7F)

--- src/gameboy/cpu/instructions.rs
+++ src/gameboy/cpu/instructions.rs
@@ -131,6 +131,22 @@ macro_rules! instruction {
 }
 
 macro_rules! ld_r16_u16 {
+    (sp) => {
+        instruction! {
+            InstructionStep::Standard(|cpu, bus| {
+                cpu.operand8 = cpu.fetch(bus);
+                // TODO: below line looks wrong, why are we shifting cpu.sp 8 bits left?
+                cpu.sp = ((cpu.sp << 8) & 0xFF00) | cpu.operand8 as u16;
+                InstructionState::InProgress
+            }),
+            InstructionStep::Standard(|cpu, bus| {
+                let higher_bits = cpu.fetch(bus);
+                cpu.sp = (higher_bits as u16) << 8 | cpu.operand8 as u16;
+                InstructionState::Finished
+            })
+        }
+    };
+
     ($reg:ident) => {
         instruction! {
             InstructionStep::Standard(|cpu, bus| {
@@ -165,10 +181,30 @@ macro_rules! add_hl_r16 {
 }
 
 macro_rules! ld_mem_a {
+    (hlplus) => {
+        instruction! {
+            InstructionStep::Standard(|cpu, bus| {
+                bus.write_u8(cpu.hl.into(), cpu.af.hi);
+                cpu.hl += 1;
+                InstructionState::Finished
+            })
+        }
+    };
+
+    (hlminus) => {
+        instruction! {
+            InstructionStep::Standard(|cpu, bus| {
+                bus.write_u8(cpu.hl.into(), cpu.af.hi);
+                cpu.hl -= 1;
+                InstructionState::Finished
+            })
+        }
+    };
+
     ($reg:ident) => {
         instruction! {
             InstructionStep::Standard(|cpu, bus| {
-                cpu.af.hi = bus.read_u8(cpu.$reg.into());
+                bus.write_u8(cpu.$reg.into(), cpu.af.hi);
                 InstructionState::Finished
             })
         }
@@ -240,6 +276,26 @@ macro_rules! ld_r8_u8 {
 }
 
 macro_rules! ld_a_mem {
+    (hlplus) => {
+        instruction! {
+            InstructionStep::Standard(|cpu, bus| {
+                cpu.af.hi = bus.read_u8(cpu.hl.into());
+                cpu.hl += 1;
+                InstructionState::Finished
+            })
+        }
+    };
+
+    (hlminus) => {
+        instruction! {
+            InstructionStep::Standard(|cpu, bus| {
+                cpu.af.hi = bus.read_u8(cpu.hl.into());
+                cpu.hl -= 1;
+                InstructionState::Finished
+            })
+        }
+    };
+
     ($reg:ident) => {
         instruction! {
             InstructionStep::Standard(|cpu, bus| {
@@ -284,6 +340,83 @@ macro_rules! jr_i8 {
             })
         }
     };
+}
+
+macro_rules! ld_r8_r8 {
+    ($dest_reg:ident,$dest_bit:ident <= HL) => {
+        instruction! {
+            InstructionStep::Standard(|cpu, bus| {
+                cpu.$dest_reg.$dest_bit = bus.read_u8(cpu.hl.into());
+                InstructionState::Finished
+            })
+        }
+    };
+
+    ($dest_reg:ident,$dest_bit:ident <= $src_reg:ident,$src_bit:ident) => {
+        instruction! {
+            InstructionStep::Instant(|cpu, _| {
+                cpu.$dest_reg.$dest_bit = cpu.$src_reg.$src_bit;
+                InstructionState::Finished
+            })
+        }
+    };
+}
+
+macro_rules! ld_memhl_r8 {
+    (HL <= $reg:ident,$bit:ident) => {
+        instruction! {
+            InstructionStep::Standard(|cpu, bus| {
+                bus.write_u8(cpu.hl.into(), cpu.$reg.$bit);
+                InstructionState::Finished
+            })
+        }
+    };
+}
+
+fn inc_hl() -> Instruction {
+    instruction! {
+        InstructionStep::Standard(|cpu, bus| {
+            cpu.temp8 = bus.read_u8(cpu.hl.into());
+            InstructionState::InProgress
+        }),
+        InstructionStep::Standard(|cpu, bus| {
+            cpu.set_flag_if_cond_else_clear((cpu.temp8 & 0x0F) == 0x0F, Flag::H);
+            cpu.temp8 = cpu.temp8.wrapping_add(1);
+            cpu.handle_z_flag(cpu.temp8);
+            cpu.clear_flag(Flag::N);
+
+            bus.write_u8(cpu.hl.into(), cpu.temp8);
+            InstructionState::Finished
+        })
+    }
+}
+
+fn dec_hl() -> Instruction {
+    instruction! {
+        InstructionStep::Standard(|cpu, bus| {
+            cpu.temp8 = bus.read_u8(cpu.hl.into());
+            InstructionState::InProgress
+        }),
+        InstructionStep::Standard(|cpu, bus| {
+            cpu.set_flag_if_cond_else_clear((cpu.temp8 & 0x0F) == 0x0F, Flag::H);
+            cpu.temp8 = cpu.temp8.wrapping_sub(1);
+            cpu.handle_z_flag(cpu.temp8);
+            cpu.set_flag(Flag::N);
+
+            bus.write_u8(cpu.hl.into(), cpu.temp8);
+            InstructionState::Finished
+        })
+    }
+}
+
+fn ld_hlmem_u8() -> Instruction {
+    instruction! {
+        fetch8,
+        InstructionStep::Standard(|cpu, bus| {
+            bus.write_u8(cpu.hl.into(), cpu.operand8);
+            InstructionState::Finished
+        })
+    }
 }
 
 impl Cpu {
@@ -378,6 +511,117 @@ impl InstructionCache {
 
             0x20 => jr_i8!(NZ),
             0x21 => ld_r16_u16!(hl),
+            0x22 => ld_mem_a!(hlplus),
+            0x23 => inc_r16!(hl),
+            0x24 => inc_r8!(hl, hi),
+            0x25 => dec_r8!(hl, hi),
+            0x26 => ld_r8_u8!(hl, hi),
+            0x27 => instruction!(InstructionStep::Instant(|_, _| unimplemented!("DAA"))),
+            0x28 => jr_i8!(Z),
+            0x29 => add_hl_r16!(hl),
+            0x2A => ld_a_mem!(hlplus),
+            0x2B => dec_r16!(hl),
+            0x2C => inc_r8!(hl, lo),
+            0x2D => dec_r8!(hl, lo),
+            0x2E => ld_r8_u8!(hl, lo),
+            0x2F => instruction!(InstructionStep::Instant(|_, _| unimplemented!("CPL"))),
+
+            0x30 => jr_i8!(NC),
+            0x31 => ld_r16_u16!(sp),
+            0x32 => ld_mem_a!(hlminus),
+            0x33 => inc_r16!(sp),
+            0x34 => inc_hl(),
+            0x35 => dec_hl(),
+            0x36 => ld_hlmem_u8(),
+            0x37 => instruction!(InstructionStep::Instant(|_, _| unimplemented!("SCF"))),
+            0x38 => jr_i8!(C),
+            0x39 => add_hl_r16!(sp),
+            0x3A => ld_a_mem!(hlminus),
+            0x3B => dec_r16!(sp),
+            0x3C => inc_r8!(af, hi),
+            0x3D => dec_r8!(af, hi),
+            0x3E => ld_r8_u8!(af, hi),
+            0x3F => instruction!(InstructionStep::Instant(|_, _| unimplemented!("CCF"))),
+
+            // ld b, r8
+            0x40 => ld_r8_r8!(bc, hi <= bc, hi),
+            0x41 => ld_r8_r8!(bc, hi <= bc, lo),
+            0x42 => ld_r8_r8!(bc, hi <= de, hi),
+            0x43 => ld_r8_r8!(bc, hi <= de, lo),
+            0x44 => ld_r8_r8!(bc, hi <= hl, hi),
+            0x45 => ld_r8_r8!(bc, hi <= hl, lo),
+            0x46 => ld_r8_r8!(bc, hi <= HL),
+            0x47 => ld_r8_r8!(bc, hi <= af, hi),
+
+            // ld c, r8
+            0x48 => ld_r8_r8!(bc, lo <= bc, hi),
+            0x49 => ld_r8_r8!(bc, lo <= bc, lo),
+            0x4A => ld_r8_r8!(bc, lo <= de, hi),
+            0x4B => ld_r8_r8!(bc, lo <= de, lo),
+            0x4C => ld_r8_r8!(bc, lo <= hl, hi),
+            0x4D => ld_r8_r8!(bc, lo <= hl, lo),
+            0x4E => ld_r8_r8!(bc, lo <= HL),
+            0x4F => ld_r8_r8!(bc, lo <= af, hi),
+
+            // ld d, r8
+            0x50 => ld_r8_r8!(de, hi <= bc, hi),
+            0x51 => ld_r8_r8!(de, hi <= bc, lo),
+            0x52 => ld_r8_r8!(de, hi <= de, hi),
+            0x53 => ld_r8_r8!(de, hi <= de, lo),
+            0x54 => ld_r8_r8!(de, hi <= hl, hi),
+            0x55 => ld_r8_r8!(de, hi <= hl, lo),
+            0x56 => ld_r8_r8!(de, hi <= HL),
+            0x57 => ld_r8_r8!(de, hi <= af, hi),
+
+            // ld e, r8
+            0x58 => ld_r8_r8!(de, lo <= bc, hi),
+            0x59 => ld_r8_r8!(de, lo <= bc, lo),
+            0x5A => ld_r8_r8!(de, lo <= de, hi),
+            0x5B => ld_r8_r8!(de, lo <= de, lo),
+            0x5C => ld_r8_r8!(de, lo <= hl, hi),
+            0x5D => ld_r8_r8!(de, lo <= hl, lo),
+            0x5E => ld_r8_r8!(de, lo <= HL),
+            0x5F => ld_r8_r8!(de, lo <= af, hi),
+
+            // ld h, r8
+            0x60 => ld_r8_r8!(hl, hi <= bc, hi),
+            0x61 => ld_r8_r8!(hl, hi <= bc, lo),
+            0x62 => ld_r8_r8!(hl, hi <= de, hi),
+            0x63 => ld_r8_r8!(hl, hi <= de, lo),
+            0x64 => ld_r8_r8!(hl, hi <= hl, hi),
+            0x65 => ld_r8_r8!(hl, hi <= hl, lo),
+            0x66 => ld_r8_r8!(hl, hi <= HL),
+            0x67 => ld_r8_r8!(hl, hi <= af, hi),
+
+            // ld l, r8
+            0x68 => ld_r8_r8!(hl, lo <= bc, hi),
+            0x69 => ld_r8_r8!(hl, lo <= bc, lo),
+            0x6A => ld_r8_r8!(hl, lo <= de, hi),
+            0x6B => ld_r8_r8!(hl, lo <= de, lo),
+            0x6C => ld_r8_r8!(hl, lo <= hl, hi),
+            0x6D => ld_r8_r8!(hl, lo <= hl, lo),
+            0x6E => ld_r8_r8!(hl, lo <= HL),
+            0x6F => ld_r8_r8!(hl, lo <= af, hi),
+
+            // ld (HL), r8
+            0x70 => ld_memhl_r8!(HL <= bc, hi),
+            0x71 => ld_memhl_r8!(HL <= bc, lo),
+            0x72 => ld_memhl_r8!(HL <= de, hi),
+            0x73 => ld_memhl_r8!(HL <= de, lo),
+            0x74 => ld_memhl_r8!(HL <= hl, hi),
+            0x75 => ld_memhl_r8!(HL <= hl, lo),
+            0x76 => instruction!(InstructionStep::Instant(|_, _| unimplemented!("HALT"))),
+            0x77 => ld_memhl_r8!(HL <= af, hi),
+
+            // ld a, r8
+            0x78 => ld_r8_r8!(af, hi <= bc, hi),
+            0x79 => ld_r8_r8!(af, hi <= bc, lo),
+            0x7A => ld_r8_r8!(af, hi <= de, hi),
+            0x7B => ld_r8_r8!(af, hi <= de, lo),
+            0x7C => ld_r8_r8!(af, hi <= hl, hi),
+            0x7D => ld_r8_r8!(af, hi <= hl, lo),
+            0x7E => ld_r8_r8!(af, hi <= HL),
+            0x7F => ld_r8_r8!(af, hi <= af, hi),
 
             _ => instruction!(InstructionStep::Instant(|_, _| unimplemented!(""))),
         };

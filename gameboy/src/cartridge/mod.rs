6ba9bc540f095101db6cc41a9fdb0e56ddd3cbca
Ruben <chickenzrage@gmail.com>
2021-11-27 20:14:58 +0000

    Implement fetching cgb compatibility mode

--- gameboy/src/cartridge/mod.rs
+++ gameboy/src/cartridge/mod.rs
@@ -9,7 +9,30 @@ use std::{
 
 use crate::cartridge::{mbc1::Mbc1, rom::Rom};
 
+#[derive(Clone, Copy)]
+pub enum CgbCompatibility {
+    None,
+    CgbOnly,
+    CgbAndDmg,
+}
+
+impl CgbCompatibility {
+    pub fn is_cgb_mode(&self) -> bool {
+        matches!(self, &CgbCompatibility::CgbOnly)
+    }
+}
+
+fn default_get_cgb_compatibility(cart: &impl Cartridge) -> CgbCompatibility {
+    match cart.read_rom(0x143) {
+        0x80 => CgbCompatibility::CgbAndDmg,
+        0xC0 => CgbCompatibility::CgbOnly,
+        _ => CgbCompatibility::None,
+    }
+}
+
 pub trait Cartridge {
+    fn get_cgb_compatibility(&self) -> CgbCompatibility;
+
     fn read_rom(&self, addr: u16) -> u8;
     fn write_rom(&mut self, addr: u16, value: u8);
 

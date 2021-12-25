44ab56a0d657d0693b94343d79d2423990533674
Ruben <chickenzrage@gmail.com>
2021-12-25 02:02:08 +0000

    Improve MBC3 code

--- gameboy/src/cartridge/mbc3.rs
+++ gameboy/src/cartridge/mbc3.rs
@@ -13,6 +13,7 @@ pub struct Mbc3 {
     is_ram_rtc_enabled: bool,
     current_rom_bank: usize,
     current_ram_bank: usize,
+    rom_bank_mask: u8,
 
     rom_banks: Vec<[u8; 0x4000]>,
     ram_banks: Vec<[u8; 0x2000]>,
@@ -40,6 +41,17 @@ impl Mbc3 {
             rom_banks.push(bank);
         }
 
+        let rom_bank_mask = match num_rom_banks - 1 {
+            0..=1 => 0b0000_0001,
+            2..=3 => 0b0000_0011,
+            4..=7 => 0b0000_0111,
+            8..=15 => 0b0000_1111,
+            16..=31 => 0b0001_1111,
+            32..=63 => 0b0011_1111,
+            64..=127 => 0b0111_1111,
+            _ => 0b1111_1111,
+        };
+
         let mut ram_banks = Vec::new();
         let save_file_path = get_save_file_path_from_rom_path(path);
 
@@ -50,6 +62,7 @@ impl Mbc3 {
             is_ram_rtc_enabled: false,
             current_rom_bank: 1,
             current_ram_bank: 0,
+            rom_bank_mask,
 
             rtc_regs: [0; 5],
             rtc_banked: false,
@@ -75,31 +88,35 @@ impl Drop for Mbc3 {
 
 impl Cartridge for Mbc3 {
     fn read_rom(&self, addr: u16) -> u8 {
-        match addr & 0xF000 {
-            0x0000 | 0x1000 | 0x2000 | 0x3000 => self.rom_banks[0][addr as usize],
-
-            0x4000 | 0x5000 | 0x6000 | 0x7000 => {
-                self.rom_banks[self.current_rom_bank][(addr - 0x4000) as usize]
-            }
-
+        match addr {
+            0x0000..=0x3FFF => self.rom_banks[0][addr as usize],
+            0x4000..=0x7FFF => self.rom_banks[self.current_rom_bank][(addr - 0x4000) as usize],
             _ => panic!(),
         }
     }
 
     fn write_rom(&mut self, addr: u16, value: u8) {
-        match addr & 0xF000 {
-            0x0000 | 0x1000 => {
-                self.is_ram_rtc_enabled = (value & 0x0F) == 0x0A;
+        match addr {
+            0x0000..=0x1FFF => {
+                let value = value & 0x0F;
+
+                if value == 0x0A {
+                    self.is_ram_rtc_enabled = true;
+                } else if value == 0x0 {
+                    self.is_ram_rtc_enabled = false;
+                }
             }
 
-            0x2000 | 0x3000 => {
+            0x2000..=0x3FFF => {
                 self.current_rom_bank = value as usize;
                 if self.current_rom_bank == 0 {
                     self.current_rom_bank = 1
                 }
+
+                self.current_rom_bank &= self.rom_bank_mask as usize;
             }
 
-            0x4000 | 0x5000 => {
+            0x4000..=0x5FFF => {
                 if value <= 0x03 {
                     self.current_ram_bank = value as usize;
                     self.rtc_banked = false;
@@ -108,7 +125,7 @@ impl Cartridge for Mbc3 {
                 }
             }
 
-            0x6000 | 0x7000 => {
+            0x6000..=0x7FFF => {
                 if self.prev_latch_val == 0x00 && value == 0x01 {
                     let now = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
 

5265f9836a3711d298b2c8ad5fc3f15919da0f21
Ruben <chickenzrage@gmail.com>
2022-07-05 23:54:42 +0100

    improve ci more

---
+++ scripts/test_ci.py
@@ -0,0 +1,28 @@
+import os
+import subprocess
+
+ignored_tests = [
+    "intr_2_mode0_timing_sprites",
+    "intr_2_oam_ok_timing",
+    "stat_lyc_onoff",
+    "mbc1::multicart_rom_8Mb"
+]
+
+def transform(test_name):
+    return "test({t})".format(t=test_name)
+
+ignored_tests = list(map(transform, ignored_tests))
+
+filter_exp = "not ({tests})".format(tests=" | ".join(ignored_tests))
+
+os.environ["NEXTEST_EXPERIMENTAL_FILTER_EXPR"] = "1"
+
+subprocess.call([
+    'cargo',
+    'nextest',
+    'run',
+    '--profile',
+    'ci',
+    '-E',
+    filter_exp
+], shell=True)

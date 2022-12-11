99a1325f46bd2d5b249b676bec4157318fd50da1
Ruben <chickenzrage@gmail.com>
2022-12-11 01:00:27 +0000

    feat: add compressed, bitpacked snapshots

--- frontends/frontend/src/emu_thread.rs
+++ frontends/frontend/src/emu_thread.rs
@@ -3,11 +3,87 @@ use std::time::Duration;
 use common::loop_helper::LoopHelper;
 use crossbeam::channel::{Receiver, Sender};
 use gameboy::{GameBoy, SPEED};
+use lz4_flex::{compress_prepend_size, decompress_size_prepended};
 use ringbuffer::{ConstGenericRingBuffer, RingBuffer, RingBufferExt, RingBufferWrite};
 
 use crate::msgs::{MsgFromGb, MsgToGb};
 
 const FPS_REPORT_RATE_MS: u64 = 500;
+
+struct BitPackedState {
+    bytes: usize,
+    data: Vec<u64>,
+}
+
+impl BitPackedState {
+    const CHUNK_SIZE: usize = std::mem::size_of::<u64>() / std::mem::size_of::<u8>();
+
+    fn pack(state: Vec<u8>) -> Self {
+        let bytes = state.len();
+        let data = state
+            .chunks(Self::CHUNK_SIZE)
+            .map(|chunk| {
+                let mut pack: u64 = 0;
+                chunk
+                    .iter()
+                    .enumerate()
+                    .for_each(|(i, byte)| pack |= (*byte as u64) << (i * 8));
+                pack
+            })
+            .collect::<Vec<u64>>();
+
+        log::info!("raw: {}, packed: {}", bytes, data.len());
+
+        Self { bytes, data }
+    }
+
+    fn unpack(&self) -> Vec<u8> {
+        let remainder = self.bytes % Self::CHUNK_SIZE;
+        let remainding = remainder != 0;
+        let take = self.data.len() - usize::from(remainding);
+
+        let mut unpacked = self
+            .data
+            .iter()
+            .take(take)
+            .flat_map(|packed| {
+                let mut packed_bytes = [0u8; Self::CHUNK_SIZE];
+                packed_bytes.iter_mut().enumerate().for_each(|(i, byte)| {
+                    *byte = (packed >> (i * 8)) as u8;
+                });
+
+                packed_bytes
+            })
+            .collect::<Vec<u8>>();
+
+        if remainding {
+            let mut remainding = Vec::new();
+            let packed = self.data.last().unwrap();
+            for i in 0..remainder {
+                remainding.push((packed >> (i * 8)) as u8);
+            }
+
+            unpacked.append(&mut remainding);
+        }
+
+        unpacked
+    }
+}
+
+fn take_snapshot(gb: &GameBoy) -> BitPackedState {
+    let encoded = rmp_serde::to_vec(&gb).unwrap();
+    //let encoded = bincode::serialize(&gb).unwrap();
+    let compressed = compress_prepend_size(&encoded);
+    BitPackedState::pack(compressed)
+}
+
+fn apply_snapshot(gb: &mut GameBoy, snapshot: &BitPackedState) {
+    let unpacked = snapshot.unpack();
+    let decompressed = decompress_size_prepended(&unpacked).unwrap();
+    //let state: GameBoy = bincode::deserialize(&decompressed).unwrap();
+    let state: GameBoy = rmp_serde::from_slice(&decompressed).unwrap();
+    gb.load_snapshot(state);
+}
 
 pub fn new(rom: Option<Vec<u8>>, bios: Option<Vec<u8>>) -> (Sender<MsgToGb>, Receiver<MsgFromGb>) {
     let (s_to_gb, r_from_ui) = crossbeam::channel::bounded::<MsgToGb>(32);
@@ -30,7 +106,8 @@ pub fn new(rom: Option<Vec<u8>>, bios: Option<Vec<u8>>) -> (Sender<MsgToGb>, Rec
             .expect("Internal error: unable to construct emulator instance");
 
         let mut turbo = false;
-        let mut snapshot: Option<Vec<u8>> = None;
+        let mut snapshot: Option<BitPackedState> = None;
+        let mut history = Vec::new();
 
         let mut loop_helper = LoopHelper::new(FPS_REPORT_RATE_MS, SPEED);
 
@@ -51,14 +128,12 @@ pub fn new(rom: Option<Vec<u8>>, bios: Option<Vec<u8>>) -> (Sender<MsgToGb>, Rec
                         last_8_frames.clear();
                     }
                     MsgToGb::SaveSnapshot => {
-                        let buf = rmp_serde::to_vec(&gb).unwrap();
-                        log::info!("Snapshot taken: {}", buf.len());
-                        snapshot = Some(buf);
+                        let state = take_snapshot(&gb);
+                        snapshot = Some(state);
                     }
                     MsgToGb::LoadSnapshot => {
-                        if let Some(snapshot) = &snapshot {
-                            let snapshot: GameBoy = rmp_serde::from_slice(snapshot).unwrap();
-                            gb.load_snapshot(snapshot);
+                        if let Some(state) = &snapshot {
+                            apply_snapshot(&mut gb, state);
                             log::info!("Loaded snapshot")
                         }
                     }
@@ -83,6 +158,8 @@ pub fn new(rom: Option<Vec<u8>>, bios: Option<Vec<u8>>) -> (Sender<MsgToGb>, Rec
                 last_8_frames.push(fps);
                 let fps = last_8_frames.iter().sum::<f64>() / last_8_frames.len() as f64;
                 let _ = s.try_send(MsgFromGb::Fps(fps));
+
+                history.push(take_snapshot(&gb));
             }
 
             if !turbo {
@@ -92,4 +169,27 @@ pub fn new(rom: Option<Vec<u8>>, bios: Option<Vec<u8>>) -> (Sender<MsgToGb>, Rec
     });
 
     (s_to_gb, r_from_gb)
+}
+
+#[cfg(test)]
+mod test {
+    use super::BitPackedState;
+
+    #[test]
+    fn pack_and_unpack() {
+        let arr = vec![0u8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        let packed = BitPackedState::pack(arr.clone());
+
+        for i in &packed.data {
+            println!("{:#064b}", i);
+        }
+
+        let unpacked = packed.unpack();
+
+        for i in &unpacked {
+            println!("{:#010b}", *i);
+        }
+
+        assert_eq!(arr, unpacked);
+    }
 }

052f80d4309584d3bf81c943b188b1a8045f3df8
Ruben <ostrichboy123@gmail.com>
2024-01-29 18:36:07 +0000

    chore: bump partyboy-frontend crates (#23)
    * chore: update libs
    * refactor: improve emu_thread.rs code

--- partyboy-frontend/src/emu_thread.rs
+++ partyboy-frontend/src/emu_thread.rs
@@ -2,13 +2,13 @@ use std::{collections::VecDeque, time::Duration};
 
 use cpal::{
     traits::{DeviceTrait, HostTrait, StreamTrait},
-    SampleRate, StreamConfig,
+    SampleRate, Stream, StreamConfig,
 };
 use crossbeam::channel::{Receiver, Sender};
 use lz4_flex::{compress_prepend_size, decompress_size_prepended};
 use partyboy_common::loop_helper::LoopHelper as ReportHelper;
 use partyboy_core::{GameBoy, SPEED};
-use ringbuffer::{ConstGenericRingBuffer, RingBuffer, RingBufferExt, RingBufferWrite};
+use ringbuffer::{ConstGenericRingBuffer, RingBuffer};
 
 use crate::msgs::{MsgFromGb, MsgToGb};
 
@@ -26,62 +26,86 @@ fn apply_snapshot(gb: &mut GameBoy, snapshot: &[u8]) {
     gb.release_all_keys();
 }
 
+fn set_up_audio() -> (Option<Stream>, Sender<(f32, f32)>) {
+    let hosts = cpal::available_hosts().len();
+    log::debug!("{}", hosts);
+
+    let host = cpal::default_host();
+
+    let audio_devices_found = match host.output_devices() {
+        Ok(devices) => devices.count(),
+        Err(_) => 0,
+    };
+
+    log::info!("Audio devices found: {audio_devices_found}");
+
+    let (audio_s, audio_r) = crossbeam::channel::bounded::<(f32, f32)>(512 * 16);
+
+    let audio_stream = if audio_devices_found > 0 {
+        let device = host
+            .default_output_device()
+            .expect("no output device found");
+
+        log::info!("Using audio device: {}", device.name().unwrap());
+
+        let config = StreamConfig {
+            channels: 2,
+            sample_rate: SampleRate(48000),
+            buffer_size: cpal::BufferSize::Fixed(512),
+        };
+
+        let audio_stream = device
+            .build_output_stream(
+                &config,
+                move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
+                    let mut index = 0;
+                    while index < data.len() {
+                        let sample = audio_r.try_recv().ok().unwrap_or((0.0, 0.0));
+
+                        data[index] = sample.0;
+                        data[index + 1] = sample.1;
+                        index += 2;
+                    }
+                },
+                move |e| {
+                    println!("Stream error: {e:?}");
+                },
+                None,
+            )
+            .unwrap();
+
+        audio_stream.play().unwrap();
+        Some(audio_stream)
+    } else {
+        // Create a "fake audio device" thread that consumes the channel
+
+        std::thread::spawn(move || {
+            use std::time::Instant;
+
+            let mut last = Instant::now();
+            loop {
+                let now = Instant::now();
+                let samples_to_consume = ((now - last).as_secs_f64() * 48_000.0) as usize;
+
+                let _: Vec<_> = audio_r.try_iter().take(samples_to_consume).collect();
+
+                last = now;
+                std::thread::sleep(Duration::from_millis(1));
+            }
+        });
+
+        None
+    };
+
+    (audio_stream, audio_s)
+}
+
 pub fn new(rom: Option<Vec<u8>>, bios: Option<Vec<u8>>) -> (Sender<MsgToGb>, Receiver<MsgFromGb>) {
     let (s_to_gb, r_from_ui) = crossbeam::channel::bounded::<MsgToGb>(32);
     let (s_to_ui, r_from_gb) = crossbeam::channel::bounded::<MsgFromGb>(128);
 
     std::thread::spawn(move || {
-        // set up audio
-        let hosts = cpal::available_hosts().len();
-        log::debug!("{}", hosts);
-
-        let host = cpal::default_host();
-
-        let audio_devices_found = match host.output_devices() {
-            Ok(devices) => devices.count(),
-            Err(_) => 0,
-        };
-
-        log::debug!("Audio devices found: {audio_devices_found}");
-
-        let (audio_s, audio_r) = crossbeam::channel::bounded::<(f32, f32)>(512 * 16);
-
-        if audio_devices_found > 0 {
-            let device = host
-                .default_output_device()
-                .expect("no output device found");
-
-            log::info!("Using audio device: {}", device.name().unwrap());
-
-            let config = StreamConfig {
-                channels: 2,
-                sample_rate: SampleRate(48000),
-                buffer_size: cpal::BufferSize::Fixed(512),
-            };
-
-            let audio_stream = device
-                .build_output_stream(
-                    &config,
-                    move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
-                        let rx = audio_r.clone();
-
-                        let mut index = 0;
-                        while index < data.len() {
-                            let sample = rx.try_recv().ok().unwrap_or((0.0, 0.0));
-
-                            data[index] = sample.0;
-                            data[index + 1] = sample.1;
-                            index += 2;
-                        }
-                    },
-                    move |_| {},
-                )
-                .unwrap();
-
-            audio_stream.play().unwrap();
-        } else {
-            log::info!("No audio device found, dropping all audio produced");
-        }
+        let (_stream, audio_s) = set_up_audio();
 
         let (s, r) = (s_to_ui, r_from_ui);
 
@@ -96,7 +120,7 @@ pub fn new(rom: Option<Vec<u8>>, bios: Option<Vec<u8>>) -> (Sender<MsgToGb>, Rec
         }
         let mut gb = builder
             .build()
-            .expect("Internal error: unable to construct emulator instance");
+            .expect("Unable to construct emulator instance");
 
         let mut turbo = false;
         let mut snapshot: Option<Vec<u8>> = None;
@@ -105,16 +129,10 @@ pub fn new(rom: Option<Vec<u8>>, bios: Option<Vec<u8>>) -> (Sender<MsgToGb>, Rec
         let mut rewind = false;
 
         let mut report_helper = ReportHelper::new(FPS_REPORT_RATE_MS, SPEED);
-        #[allow(deprecated)]
-        let mut loop_helper = spin_sleep::LoopHelper::builder()
-            .report_interval(Duration::from_millis(500))
-            .build_with_target_rate(59.73);
 
         let mut last_8_frames = ConstGenericRingBuffer::<_, 8>::new();
 
         loop {
-            loop_helper.loop_start();
-
             // calculate how many ticks have elapsed
             let now = partyboy_common::time::now();
 
@@ -122,11 +140,11 @@ pub fn new(rom: Option<Vec<u8>>, bios: Option<Vec<u8>>) -> (Sender<MsgToGb>, Rec
             for msg in msgs {
                 match msg {
                     MsgToGb::Load => todo!(),
-                    MsgToGb::KeyDown(keys) => {
-                        log::debug!("{:?}", keys);
-                        keys.into_iter().for_each(|key| gb.key_down(key))
+                    MsgToGb::KeyDown(key) => {
+                        log::debug!("{:?}", key);
+                        gb.key_down(key);
                     }
-                    MsgToGb::KeyUp(keys) => keys.into_iter().for_each(|key| gb.key_up(key)),
+                    MsgToGb::KeyUp(key) => gb.key_up(key),
                     MsgToGb::Turbo(state) => {
                         turbo = state;
                         last_8_frames.clear();
@@ -156,41 +174,22 @@ pub fn new(rom: Option<Vec<u8>>, bios: Option<Vec<u8>>) -> (Sender<MsgToGb>, Rec
                     break 'tick_emulator;
                 }
 
-                if audio_devices_found > 0 {
-                    while audio_s.len() < 512 * 4 {
-                        let sample = gb.tick();
-                        if let Some(sample) = sample {
-                            audio_s.try_send(sample).unwrap();
-                        }
-
-                        if gb.consume_draw_flag() {
-                            let frame_msg = MsgFromGb::Frame(gb.get_frame_buffer().into());
-                            let _ = s.try_send(frame_msg);
-                            report_helper.record_frame_draw();
-
-                            // record state
-                            if !turbo {
-                                history.push_front(take_snapshot(&gb));
-                                if history.len() > 60 * 12 {
-                                    history.pop_back();
-                                }
-                            }
-                        }
+                while audio_s.len() < 512 * 4 {
+                    let sample = gb.tick();
+                    if let Some(sample) = sample {
+                        audio_s.try_send(sample).unwrap();
                     }
-                } else {
-                    loop {
-                        gb.tick();
-                        if gb.consume_draw_flag() {
-                            let frame_msg = MsgFromGb::Frame(gb.get_frame_buffer().into());
-                            let _ = s.try_send(frame_msg);
-                            report_helper.record_frame_draw();
 
-                            // record state
-                            if !turbo {
-                                history.push_front(take_snapshot(&gb));
-                                if history.len() > 60 * 12 {
-                                    history.pop_back();
-                                }
+                    if gb.consume_draw_flag() {
+                        let frame_msg = MsgFromGb::Frame(gb.get_frame_buffer().into());
+                        let _ = s.try_send(frame_msg);
+                        report_helper.record_frame_draw();
+
+                        // record state
+                        if !turbo {
+                            history.push_front(take_snapshot(&gb));
+                            if history.len() > 60 * 12 {
+                                history.pop_back();
                             }
                         }
                     }
@@ -224,10 +223,8 @@ pub fn new(rom: Option<Vec<u8>>, bios: Option<Vec<u8>>) -> (Sender<MsgToGb>, Rec
                 let _ = s.try_send(MsgFromGb::Fps(fps));
             }
 
-            if !turbo && audio_devices_found > 0 {
+            if !turbo {
                 std::thread::sleep(Duration::from_millis(1));
-            } else if !turbo && audio_devices_found == 0 {
-                loop_helper.loop_sleep();
             }
         }
     });

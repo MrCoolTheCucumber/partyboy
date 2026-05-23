a892cb5de623b387b8a46d8d89753c390ae20902
Ruben <ruben@mrcoolthecucumber.com>
2026-05-23 03:11:21 +0100

    chore: migrate to tracing stack for logging

--- partyboy-frontend/src/logging.rs
+++ partyboy-frontend/src/logging.rs
@@ -1,45 +1,50 @@
-use std::env;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter, fmt};
 
-use log::LevelFilter;
-use log4rs::{
-    append::file::FileAppender,
-    config::{Appender, Root},
-    encode::pattern::PatternEncoder,
-    Config,
-};
-
-// TODO: why am I using two logger libs?
 pub fn init_logger(enable_file_logging: bool) {
-    if env::var("RUST_LOG").is_err() {
-        // SAFETY: Single-threaded initialization before any other threads are spawned.
-        unsafe { env::set_var("RUST_LOG", "info") }
-    }
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+        EnvFilter::new("info")
+            .add_directive("wgpu_core=warn".parse().unwrap())
+            .add_directive("wgpu_hal=error".parse().unwrap())
+            .add_directive("naga=warn".parse().unwrap())
+    });
+
+    let fmt_layer = fmt::layer()
+        .with_target(true)
+        .with_thread_ids(false)
+        .with_file(false);
+
+    let registry = tracing_subscriber::registry()
+        .with(env_filter)
+        .with(fmt_layer);
 
     if enable_file_logging {
-        const LOG_PATTERN: &str = "{m}\n";
-        let logfile = FileAppender::builder()
-            .encoder(Box::new(PatternEncoder::new(LOG_PATTERN)))
-            .build("log/output.log")
-            .unwrap();
+        // Non-blocking file writer so we don't stall the emulator thread on disk I/O.
+        let file_appender = tracing_appender::rolling::never("log", "output.log");
+        let (non_blocking, guard) = tracing_appender::non_blocking(file_appender);
 
-        let config = Config::builder()
-            .appender(Appender::builder().build("logfile", Box::new(logfile)))
-            .build(
-                Root::builder()
-                    .appender("logfile")
-                    .build(LevelFilter::Debug),
-            )
-            .unwrap();
+        // Keep the guard alive for the lifetime of the program.
+        // Leaking is acceptable and common for long-running binaries.
+        std::mem::forget(guard);
 
-        log4rs::init_config(config).unwrap();
+        let file_layer = fmt::layer()
+            .with_writer(non_blocking)
+            .with_ansi(false)
+            .with_target(true);
+
+        registry.with(file_layer).init();
     } else {
-        env_logger::builder()
-            .format_timestamp(None)
-            .filter_module("wgpu_core", LevelFilter::Warn)
-            .filter_module("wgpu_hal", LevelFilter::Error)
-            .filter_module("naga", LevelFilter::Warn)
-            .init();
+        registry.init();
     }
 
-    log_panics::init();
+    install_panic_hook();
+}
+
+fn install_panic_hook() {
+    let prev_hook = std::panic::take_hook();
+    std::panic::set_hook(Box::new(move |panic_info| {
+        tracing::error!(panic = ?panic_info, "application panicked");
+
+        // Delegate to the previous hook so the usual panic output still happens
+        prev_hook(panic_info);
+    }));
 }
